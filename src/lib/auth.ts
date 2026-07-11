6a671edddd1f1b6f5176dbfa9fd0d7180d9e4faf
Anuj Programmer <pradhananuj123@gmail.com>
2026-07-11 23:48:14 +0545

    10: payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
+import {checkout, polar, portal} from "@polar-sh/better-auth"
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-
+import { polarClient } from "./polar";
 import prisma from '../lib/db';
 
 
@@ -13,4 +14,23 @@ export const auth = betterAuth({
         enabled: true,
         autoSignIn: true,
     },
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products: [
+                        {
+                            productId: "5eaf401c-5828-4cea-8395-f1fd2d4c69ca",
+                            slug: "pro",
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true,
+                }),
+                portal(),
+            ],
+        }),
+    ]
 });
